41d3a42c9b300f104e8b20dbbdd785e4d661a777
paulboco <paulboco@cableone.net>
2015-03-15 15:49:09 -0500

    Add test code

--- public/test.php
+++ public/test.php
@@ -4,9 +4,11 @@
     <li><a target="_blank" href="http://paulboco.com">http://paulboco.com</a></li>
     <li><a target="_blank" href="http://www.paulboco.com">http://www.paulboco.com</a></li>
     <li><a target="_blank" href="http://foo.paulboco.com">http://foo.paulboco.com</a></li>
+    <li><hr></li>
     <li><a target="_blank" href="http://paulboco.com:2801">http://paulboco.com:2801</a></li>
     <li><a target="_blank" href="http://foo.paulboco.com:2801">http://foo.paulboco.com:2801</a></li>
     <li><a target="_blank" href="http://rpm.paulboco.com:2801">http://rpm.paulboco.com:2801</a></li>
+    <li><hr></li>
     <li><a target="_blank" href="http://paulboco.com:2802">http://paulboco.com:2802</a></li>
     <li><a target="_blank" href="http://foo.paulboco.com:2802">http://foo.paulboco.com:2802</a></li>
     <li><a target="_blank" href="http://zpma.paulboco.com:2802">http://zpma.paulboco.com:2802</a></li>
