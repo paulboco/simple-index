d3d50899c6488b14a502d864ae19970137d86422
paulboco <paulboco@cableone.net>
2015-03-15 15:28:12 -0500

    Add attribute

--- public/index.php
+++ public/index.php
@@ -2,11 +2,11 @@
 <h2 style="color:silver;"><?= $_SERVER['DOCUMENT_ROOT'] ?></h2>
 
 <ul>
-    <li><a href="http://paulboco.com">http://paulboco.com</a></li>
-    <li><a href="http://www.paulboco.com">http://www.paulboco.com</a></li>
-    <li><a href="http://foo.paulboco.com">http://foo.paulboco.com</a></li>
-    <li><a href="http://paulboco.com:2801">http://paulboco.com:2801</a></li>
-    <li><a href="http://rpm.paulboco.com:2801">http://rpm.paulboco.com:2801</a></li>
-    <li><a href="http://paulboco.com:2802">http://paulboco.com:2802</a></li>
-    <li><a href="http://zpma.paulboco.com:2802">http://zpma.paulboco.com:2802</a></li>
+    <li><a target="_blank" href="http://paulboco.com">http://paulboco.com</a></li>
+    <li><a target="_blank" href="http://www.paulboco.com">http://www.paulboco.com</a></li>
+    <li><a target="_blank" href="http://foo.paulboco.com">http://foo.paulboco.com</a></li>
+    <li><a target="_blank" href="http://paulboco.com:2801">http://paulboco.com:2801</a></li>
+    <li><a target="_blank" href="http://rpm.paulboco.com:2801">http://rpm.paulboco.com:2801</a></li>
+    <li><a target="_blank" href="http://paulboco.com:2802">http://paulboco.com:2802</a></li>
+    <li><a target="_blank" href="http://zpma.paulboco.com:2802">http://zpma.paulboco.com:2802</a></li>
 </ul>
