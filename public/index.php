500536551b4a9ca4a1029e9d85c4c19d0ef05e4f
paulboco <paulboco@cableone.net>
2015-03-15 15:38:00 -0500

    Add test code

--- public/index.php
+++ public/index.php
@@ -1,15 +1,2 @@
 <h1>Welcome to <?= $_SERVER['HTTP_HOST'] ?></h1>
 <h2 style="color:silver;"><?= $_SERVER['DOCUMENT_ROOT'] ?></h2>
-<h2 style="color:silver;"><?= $_SERVER['HTTP_REFERER'] ?></h2>
-
-<pre><!--?= print_r($_SERVER) ?--></pre>
-
-<ul>
-    <li><a target="_blank" href="http://paulboco.com">http://paulboco.com</a></li>
-    <li><a target="_blank" href="http://www.paulboco.com">http://www.paulboco.com</a></li>
-    <li><a target="_blank" href="http://foo.paulboco.com">http://foo.paulboco.com</a></li>
-    <li><a target="_blank" href="http://paulboco.com:2801">http://paulboco.com:2801</a></li>
-    <li><a target="_blank" href="http://rpm.paulboco.com:2801">http://rpm.paulboco.com:2801</a></li>
-    <li><a target="_blank" href="http://paulboco.com:2802">http://paulboco.com:2802</a></li>
-    <li><a target="_blank" href="http://zpma.paulboco.com:2802">http://zpma.paulboco.com:2802</a></li>
-</ul>
