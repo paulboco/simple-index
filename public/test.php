efa3d5100f2a8ebbeefda8ea2619a2215dc8bbf4
paulboco <paulboco@cableone.net>
2015-03-15 15:41:05 -0500

    Add test code

---
+++ public/test.php
@@ -0,0 +1,11 @@
+<pre><!--?= print_r($_SERVER) ?--></pre>
+
+<ul>
+    <li><a target="_blank" href="http://paulboco.com">http://paulboco.com</a></li>
+    <li><a target="_blank" href="http://www.paulboco.com">http://www.paulboco.com</a></li>
+    <li><a target="_blank" href="http://foo.paulboco.com">http://foo.paulboco.com</a></li>
+    <li><a target="_blank" href="http://paulboco.com:2801">http://paulboco.com:2801</a></li>
+    <li><a target="_blank" href="http://rpm.paulboco.com:2801">http://rpm.paulboco.com:2801</a></li>
+    <li><a target="_blank" href="http://paulboco.com:2802">http://paulboco.com:2802</a></li>
+    <li><a target="_blank" href="http://zpma.paulboco.com:2802">http://zpma.paulboco.com:2802</a></li>
+</ul>
