bbcc9240ea2b0a92bc57d7e77c6a2ee1036652cb
paulboco <paulboco@cableone.net>
2015-03-15 15:24:56 -0500

    Add test links

--- public/index.php
+++ public/index.php
@@ -1,2 +1,12 @@
 <h1>Welcome to <?= $_SERVER['HTTP_HOST'] ?></h1>
-<h2 style="color:white;"><?= $_SERVER['DOCUMENT_ROOT'] ?></h2>
+<h2 style="color:silver;"><?= $_SERVER['DOCUMENT_ROOT'] ?></h2>
+
+<ul>
+    <li><a href="http://paulboco.com">http://paulboco.com</a></li>
+    <li><a href="http://www.paulboco.com">http://www.paulboco.com</a></li>
+    <li><a href="http://foo.paulboco.com">http://foo.paulboco.com</a></li>
+    <li><a href="http://paulboco.com:2801">http://paulboco.com:2801</a></li>
+    <li><a href="http://rpm.paulboco.com:2801">http://rpm.paulboco.com:2801</a></li>
+    <li><a href="http://paulboco.com:2802">http://paulboco.com:2802</a></li>
+    <li><a href="http://zpma.paulboco.com:2802">http://zpma.paulboco.com:2802</a></li>
+</ul>
